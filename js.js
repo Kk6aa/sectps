1950b130b72681c0147fbfd8ce8c07fdf14db098
Ahmed Mohsen <pc.gh1070@gmail.com>
2025-07-12 14:09:50 +0300

    finished product

--- js.js
+++ js.js
@@ -1,47 +1,69 @@
-
-humanScore = 0;
-computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 // we can get the computer choice using the Math.random method 
-
-computerTurn = Math.floor(Math.random() * 3) + 1;
-
-function getComputerChoice(computerTurn) {
-
+function getComputerChoice() {
+    let computerTurn = Math.floor(Math.random() * 3) + 1;
     if (computerTurn === 1) {
-        computerTurn = "rock";
+        return "rock";
     }
     else if (computerTurn === 2) {
-        computerTurn = "paper";
+        return "paper";
+    }
+    else if (computerTurn === 3) {
+        return "scissors" }
+}
+
+function getHumanChoice() {
+    
+
+        const humanprompt = prompt("chosse one: ").toLowerCase();
+        return humanprompt;
+    
+}
+
+function determineWinner(humanChoice, computerChoice) {
+ 
+    console.log(`you chose ${humanChoice} and the computer chose ${computerChoice}`);
+
+    if (humanChoice === computerChoice) {
+        console.log("it's a tie!");
+    }
+
+    else if (
+        (computerChoice === "paper" && humanChoice === "scissors") || 
+        (computerChoice === "scissors" && humanChoice === "rock") ||
+        (computerChoice === "rock" && humanChoice === "paper")
+    )  {
+        console.log("you win! good job habibi");
+        humanScore++;
+    } else {
+        console.log("you lose, better luck next time habibi");
+        computerScore++;
+    }
+
+}
+
+function playGame() {
+
+    for (let i = 0; i < 5; i++) {
+        console.log(`\nRound ${i + 1} of 5`);
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        determineWinner(humanSelection, computerSelection);
+
+        console.log(`current score - you ${humanScore} VS computer ${computerScore}`)
+    }
+
+    if (humanScore > computerScore) {
+        console.log("You won the game!");
+    }
+    else if (humanScore < computerScore) {
+        console.log("You lost the game!");
     }
     else {
-        computerTurn = "scissors"
-    }
-    return computerTurn;
-}
-
-humanprompt = prompt("chosse one: ")
-humanTurn = humanprompt.toLowerCase()
-
-function getHumanChoice(humanTurn) {
-
-    return humanTurn;
-}
-
-function determineWinner(getComputerChoice, getHumanChoice) {
-    switch (getComputerChoice() ,getHumanChoice()) {
-        case (getComputerChoice() == "rock" && getHumanChoice() == "scissors") : {
-            console.log("computer wins! rock beats scissors");
-        }
-
-        case (getComputerChoice() == "paper" && getHumanChoice() == "scissors") : {
-            console.log("computer wins! rock beats scissors");
-        }
-
-        case (getComputerChoice() == "scissors" && getHumanChoice() == "scissors") : {
-            console.log("computer wins! rock beats scissors");
-        }
+        console.log("It's a draw!");
     }
 }
+playGame();
 
-console.log(determineWinner(getComputerChoice, getHumanChoice));
